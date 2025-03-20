7d3fece43891f906aacf452409667c5e9c414439
rrayyyyy <91184126+rrayyyyy@users.noreply.github.com>
2025-03-20 15:24:00 +0800

    Update main35.c

--- ex3/main35.c
+++ ex3/main35.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int v = 65;  // 'A' 的 ASCII 值
+    int* p = &v; // p 指向 int 型別的變數 v
+
+    printf("Address of v: %p\n", (void*)&v);
+    printf("Address stored in p: %p\n", (void*)p);
+    printf("Address of p: %p\n", (void*)&p);
+    printf("Value of *p: %d\n", *p);
+
+    return 0;
+}
 
