871a1781d22d3c72a667f54fe06fcd5ee5e13ced
rrayyyyy <91184126+rrayyyyy@users.noreply.github.com>
2025-04-10 15:55:45 +0800

    Update main25.c

--- ex4/main25.c
+++ ex4/main25.c
@@ -6,17 +6,17 @@ int factorial(int n) {
     return n * factorial(n - 1); 
 }
 
-// 遞迴方式計算字串長度
+
 int my_strlen(char* s) {
     if (!*s) return 0;              
     return 1 + my_strlen(s + 1);     
 
 int main() {
-    // 階乘測試
+  
     int n = 5;
     int m = factorial(n);
     printf("factorial %d = %d\n", n, m); 
-    // 字串長度測試
+
     char s[] = "IU is a girl!";
     int len = my_strlen(s);
     printf("(%s) len = %d\n", s, len); 
