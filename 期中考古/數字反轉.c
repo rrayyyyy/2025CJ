5840a8139f3d4b97e557c8184e27f69f48ddc6e2
rrayyyyy <91184126+rrayyyyy@users.noreply.github.com>
2025-04-16 20:20:35 +0800

    Update 數字反轉.c

--- 期中考古/數字反轉.c
+++ 期中考古/數字反轉.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+int reverse(int num) {
+    int rev = 0;
+
+    while (num > 0) {
+        rev = rev * 10 + num % 10;  // 把最後一位加到結果後面
+        num = num / 10;             // 去掉最後一位
+    }
+
+    return rev;
+}
+
+int main() {
+    int num = 54321;
+    printf("Reverse of %d = %d\n", num, reverse(num));
+    return 0;
+}
 
