d294161a88b83bf4d3c75abf0f1d5cd83a0c0fd6
rrayyyyy <91184126+rrayyyyy@users.noreply.github.com>
2025-04-17 01:01:45 +0800

    Update hw1_1.c

--- 期中考古/hw1_1.c
+++ 期中考古/hw1_1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <string.h>
 
+void encode(char *a ,int i){
+    if (a[i]=='\0'){
+        printf("\n");
+        return;
+    }
+    char current = a[i];
+    int count = 1;
+    while (current == a[i+count] && count < 9){
+        count++;
+    }
+    printf("%c%d",current,count);
+    encode(a,i+count);
+}
+
+int main() {
+    char a[] = "AABBBCCCCddd";  // 固定字串
+    encode(a, 0);            // 從第0個字元開始處理
+    return 0;
+}
