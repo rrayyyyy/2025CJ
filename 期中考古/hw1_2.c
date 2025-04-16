1ac8384930114b7d63a3abfe7f2a4fcef6032c1d
rrayyyyy <91184126+rrayyyyy@users.noreply.github.com>
2025-04-17 01:15:26 +0800

    Update hw1_2.c

--- 期中考古/hw1_2.c
+++ 期中考古/hw1_2.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+void decode(char *a , int i){
+    if(a[i] == '\0'){
+        printf("\n");
+        return;
+    }
+    char word = a[i];
+    int count = a[i+1] - '0';
+    for(int j = 0 ; j < count ; j++){
+        printf ("%c",word);
+    }
+    i = i+2;
+    decode (a , i);
+}
+
+int main() {
+    char a[] = "A4B1C3f3";  // 壓縮過的 RLE 字串
+    decode(a, 0);        // 從索引0開始還原
+    return 0;
+}
